fcd09f6b59dd04a7ad2fc8a263827cd10aa5c7e0
Andrew Bout <anbout@devvela.com>
2017-07-03 11:54:04 +0500

    form styles

--- src/taskManager/components/form/taskForm.component.ts
+++ src/taskManager/components/form/taskForm.component.ts
@@ -5,9 +5,11 @@ import { Task }             from '../../models/task';
 @Component({
     selector: 'task-form',
     templateUrl: 'taskManager/components/form/taskForm.html',
+    styleUrls: [ 'taskManager/components/form/taskForm.css' ]
 })
 export class TaskFormComponent {
   @Input() task: Task;
+  @Input() isNewTask: boolean;
 
   getDatepickerValue(dateStr: string) {
     return dateStr ? new Date(dateStr).valueOf() : null;
